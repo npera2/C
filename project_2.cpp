2b8255cce04154054dde1804c436a9f2aa590906
npera2 <115074370+npera2@users.noreply.github.com>
2023-03-28 19:45:36 +0800

    figure out a factorial

---
+++ project_2.cpp
@@ -0,0 +1,14 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a, i, sum = 1;
+	printf("aֵ");
+	scanf("%d", &a);
+	for (i = 1; i <= a; i++)
+	{
+		sum *= i;
+	}
+	printf("aĽ׳=%d", sum);
+	return 0;
+}
